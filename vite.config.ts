170b1a48a7e35d21a6d5a506596ead85bccbf844
andygggg <andyhello31468@gamil.com>
2025-06-09 14:13:59 +0800

    fix:test jenkins2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  // react_shoppingstore github deploy
   base: process.env.NODE_ENV === "production" ? "/" : "/",
   plugins: [react()],
   build: {
